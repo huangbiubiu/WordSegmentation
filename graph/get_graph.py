627d1c8243cfb516d55f0be53989a507c313c097
Huang Yuheng <hyhlryf@gmail.com>
2018-11-29 21:22:13 +0800

    forward graph temp#1

--- graph/get_graph.py
+++ graph/get_graph.py
@@ -1,23 +1,79 @@
-def get_graph(sentence, word_dict: set):
-    graph = {}
-    for i in range(len(sentence)):
-        graph[i] = [i]
-        for j in range(i + 1, len(sentence)):
-            word = sentence[i:j + 1]
-            if word in word_dict:
-                graph[i].append(j)
-    return graph
+import numpy as np
+from scipy.sparse import csc_matrix
+
+from probability.Ngram import Ngram
 
 
-def construct_dict(path: str) -> set:
-    with open(path, encoding='utf8') as f:
-        return set(f.readline().split())
+class Graph:
+    def __init__(self, dict_path: str, corpus_path: str, ngram_size: int):
+        self.ngram_size = ngram_size
+
+        print(f"building {ngram_size}-gram language model")
+        self.ngram = Ngram(ngram_size, corpus_path)
+
+        print("building word dict")
+        self.word_dict = self.__construct_dict(dict_path)
+
+        print("initialization completed.")
+
+    def get_graph(self, sentence):
+        sentence = f"S {sentence} E"
+
+        graph = csc_matrix(np.eye(len(sentence)))
+        for i in range(len(sentence)):
+            for j in range(i + 1, len(sentence)):
+                word = sentence[i:j + 1]
+                if word in self.word_dict:
+                    graph[i, j] = True
+        return graph
+
+    @staticmethod
+    def __construct_dict(path: str) -> set:
+        with open(path, encoding='utf8') as f:
+            word_dict = set(f.readline().split())
+            word_dict.add('S')
+            word_dict.add('E')
+            return word_dict
+
+    @staticmethod
+    def get_previous_index(index: int, graph) -> list:
+        return graph[:, index]
+
+    @staticmethod
+    def get_next_index(index: int, graph) -> list:
+        return graph[index, :]
+
+    @staticmethod
+    def get_previous_n(current_index: int, n: int, graph) -> list:
+        previous = [[i] for i in list(Graph.get_previous_index(current_index, graph))]
+        for _ in range(n - 1):
+            new_previous = []
+            for path in previous:
+                for p in Graph.get_previous_index(path[-1], graph):
+                    new_previous.append(path.append(p))
+
+        return previous
+
+    def calc(self, sentence):
+        graph = self.get_graph(sentence)
+
+        route = dict()
+        n = len(sentence)
+
+        for n in range(1, n):
+            pass
+
+        return route
 
 
 def main():
     dict_path: str = "/home/hyh/projects/CLProject/WordSegmentation/data/train.dict"
-    word_dict = construct_dict(dict_path)
-    g = get_graph("去北京大学玩", word_dict)
+    corpus_path: str = "/home/hyh/projects/CLProject/WordSegmentation/data/train"
+    graph = Graph(dict_path, corpus_path, 3)
+    g = graph.calc("去北京大学玩")
+
+    print(g)
+
     pass
 
 
--- probability/Ngram.py
+++ probability/Ngram.py
@@ -4,22 +4,40 @@ import numpy as np
 
 
 class Ngram:
-    def __init__(self, ngram_size):
+    def __init__(self, ngram_size, train_path: str):
         self.ngram_size: int = ngram_size
         self.vectorizer: sklearn.feature_extraction.text.CountVectorizer = sklearn.feature_extraction.text.CountVectorizer(
             ngram_range=(self.ngram_size, self.ngram_size))
-        self.matrix: csr_matrix = None
 
-    def train(self, corpus: list) -> None:
-        self.matrix: csr_matrix = self.vectorizer.fit_transform(corpus)
-        self.matrix = self.matrix.sum(axis=0).astype(np.float32)
+        corpus: list = self.__load_corpus(train_path)
+        self.matrix: csr_matrix = self.__train(corpus)
 
-        self.matrix = self.matrix / self.matrix.sum()  # normalize the probability
+    @staticmethod
+    def __load_corpus(path: str) -> list:
+        with open(path, encoding='utf8') as file:
+            lines = file.readlines()
+
+        lines = list(map(Ngram.process_line, lines))  # remove whitespaces in corpus
+
+        return lines
+
+    @staticmethod
+    def process_line(line: str) -> str:
+        line = f"S {line} E"
+        return line
+
+    def __train(self, corpus: list) -> csr_matrix:
+        matrix: csr_matrix = self.vectorizer.fit_transform(corpus)
+        matrix = matrix.sum(axis=0).astype(np.float32)
+
+        matrix = matrix / matrix.sum()  # normalize the probability
         # TODO smoothing the prob
 
+        return matrix
+
     def probability(self, word: str):
-        if len(word.split()) != self.ngram_size:
-            raise ValueError(f"Sentence length should be {self.ngram_size}")
+        # if len(word.split()) != self.ngram_size:
+        #     raise ValueError(f"Sentence length should be {self.ngram_size}")
         if word in self.vectorizer.vocabulary_:
             index = self.vectorizer.vocabulary_[word]
             return self.matrix[index]
@@ -29,11 +47,8 @@ class Ngram:
 
 def main():
     path = "/home/hyh/projects/CLProject/WordSegmentation/data/train"
-    with open(path, encoding='utf8') as file:
-        lines = file.readlines()
 
-    model = Ngram(3)
-    model.train(lines)
+    model = Ngram(3, path)
 
     print(list(model.vectorizer.vocabulary_.keys())[:100])  # todo for debug
 
